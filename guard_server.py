eb82ac31196546b87589fd97a9ff2fdc12b15ee1
Steve Henderson <steven.j.henderson@gmail.com>
2022-11-19 21:22:17 -0500

    Adding

--- guard_server.py
+++ guard_server.py
@@ -1,135 +1,172 @@
+#!/usr/bin/env python
+
+
+#
+#  This code was copied from:  https://github.com/runapp/flexproxy
+#  And then modified.
+#
+import logging
+import os
+import sys
+import socket
+import asyncio
+
+if sys.platform == 'win32':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+import tornado.httpserver
 import tornado.ioloop
-import maproxy.proxyserver
-import maproxy.session
-import string   # for the "filter"
+import tornado.iostream
+import tornado.web
+import tornado.httpclient
+import tornado.httputil
 from tornado.options import define, options
 
-define("interface", default="0.0.0.0", help="interface")
+define('port', default=8875, help='Port the proxy server runs on')
+define('bind', default='127.0.0.1', help='Address the proxy server binds to')
+define('up_host', default=None, type=str, help='Upstream proxy host')
+define('up_port', default=0, help='Upstream proxy port')
+
+logger = logging.getLogger('tornado_proxy')
+
+__all__ = ['ProxyHandler', 'run_proxy']
 
 
-class LoggingSession(maproxy.session.Session):
+def fetch_request(url, up_proxy, **kwargs):
+    if up_proxy:
+        logger.info('Forward request via upstream proxy %s:%d', *up_proxy)
+        tornado.httpclient.AsyncHTTPClient.configure(
+            'tornado.curl_httpclient.CurlAsyncHTTPClient')
+        kwargs['proxy_host'] = up_proxy[0]
+        kwargs['proxy_port'] = up_proxy[1]
+    logger.info("url: {}".format(url))
+    req = tornado.httpclient.HTTPRequest(url, **kwargs)
+    client = tornado.httpclient.AsyncHTTPClient(defaults=dict(request_timeout=180))
+    return client.fetch(req, raise_error=True)
+
+
+async def pipe(f: tornado.iostream.IOStream, t: tornado.iostream.IOStream):
+    try:
+        while True:
+            logger.info("aaa")
+            a = await f.read_bytes(16384, partial=True)
+            await t.write(a)
+    except tornado.iostream.StreamClosedError as e:
+        pass
+
+
+class ProxyHandler(tornado.web.RequestHandler):
+    SUPPORTED_METHODS = ['GET', 'POST', 'CONNECT']
+    proxy = None
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if options.up_host and options.up_port:
+            self.proxy = (options.up_host, options.up_port)
+            logger.info("Upstream proxy set to %s:%d", *self.proxy)
+
+    def compute_etag(self):
+        return None  # disable tornado Etag
+
+    async def get(self):
+        logger.info('Handle %s request to %s', self.request.method, self.request.uri)
+        logger.info('proxy {}'.format(self.proxy))
+        logger.info('headers {}'.format(self.request.headers))
+        body = self.request.body
+        if not body:
+            body = None
+        try:
+            if 'Proxy-Connection' in self.request.headers:
+                del self.request.headers['Proxy-Connection']
+            response = await fetch_request(
+                self.request.uri,
+                self.proxy,
+                method=self.request.method, body=body,
+                headers=self.request.headers, follow_redirects=False,
+                allow_nonstandard_methods=True)
+        except tornado.httpclient.HTTPError as e:
+            logger.error("Ooof we hit an error: {}".format(e))
+            if hasattr(e, 'response') and e.response:
+                pass
+            else:                
+                self.set_status(500)
+                self.write('Internal server error:\n' + str(e))
+                return
+        except ConnectionRefusedError as e:
+            self.set_status(502)
+            self.write('Remote connection refused.')
+            return
+
+        if (response.error and not
+                isinstance(response.error, tornado.httpclient.HTTPError)):
+            self.set_status(500)
+            self.write('Internal server error:\n' + str(response.error))
+        else:
+            self.set_status(response.code, response.reason)
+            self._headers = tornado.httputil.HTTPHeaders()  # clear tornado default header
+
+            for header, v in response.headers.get_all():
+                if header not in ('Content-Length', 'Transfer-Encoding', 'Content-Encoding', 'Connection'):
+                    self.add_header(header, v)  # some header appear multiple times, eg 'Set-Cookie'
+
+            if response.body:
+                self.set_header('Content-Length', len(response.body))
+                self.write(response.body)
+
+    async def post(self):
+        return self.get()
+
+    async def connect(self):
+        logger.info('Start CONNECT to %s', self.request.uri)
+        host, port = self.request.uri.split(':')  # Here the uri only contains "host:port" for CONNECT requests.
+        client: tornado.iostream.IOStream = self.request.connection.stream
+
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
+        upstream = tornado.iostream.IOStream(s)
+
+        if self.proxy:
+            await upstream.connect(self.proxy)
+            # It's safe to encode as ascii, as non-ascii domains are already encoded.
+            await upstream.write(b'CONNECT %s HTTP/1.1\r\n' % self.request.uri.encode('ascii'))
+            await upstream.write(b'Host: %s\r\n' % host.encode('ascii'))
+            await upstream.write(b'Proxy-Connection: Keep-Alive\r\n\r\n')
+            data = await upstream.read_until(b'\r\n\r\n')
+            if data:
+                first_line = data.splitlines()[0]
+                _, status, _ = first_line.split(None, 2)
+                status = int(status)
+            else:
+                status = -1
+            if status != 200:
+                self.set_status(status)
+                return
+
+            logger.info('Connected to upstream proxy %s:%d', *self.proxy)
+        else:
+            await upstream.connect((host, int(port)))
+
+        logger.info('CONNECT tunnel established to %s', self.request.uri)
+        try:
+            await client.write(b'HTTP/1.0 200 Connection established\r\n\r\n')
+        except tornado.iostream.StreamClosedError as e:
+            return
+        await asyncio.gather(pipe(upstream, client), pipe(client, upstream))
+
+
+def run_proxy(port, address, start_ioloop=True):
     """
-    This class simply overrides the major "session" functions of the parent-class.
-    The idea is very simple: every time the proxy has done "something" , we will
-    "intercept" the  call , print the data and .
-    Note that the actually code is very small. all my comments and explanations taking too much space !
-
-    There are 6 functions that we would like to monitor:
-    -  new_connection      : New session
-    -  on_p2s_done_connect : When the session is connected to the server
-    -  on_c2p_done_read    : C->S data
-    -  on_p2s_done_read    : C<-S data
-    -  on_c2p_close        : Client closes he connection
-    -  on_p2s_close        : Server closes he connection
-    
+    Run proxy on the specified port. If start_ioloop is True (default),
+    the tornado IOLoop will be started immediately.
     """
-    
-    # Class variable: counter for the number of connections
-    running_counter=0
-    
-    
-    def __init__(self,*args,**kwargs):
-        """
-        Currently overriding the "__init__" is not really required since the parent's 
-        __init__ is doing absolutely nothing, but it is a good practice for
-        the future... (future updates)
-        """
-        super(LoggingSession,self).__init__(*args,**kwargs)
+    app = tornado.web.Application([
+        (r'.*', ProxyHandler),
+    ])
+    app.listen(port, address)
+    ioloop = tornado.ioloop.IOLoop.instance()
+    if start_ioloop:
+        ioloop.start()
 
 
-    #def new_connection(self,stream : tornado.iostream.IOStream ,address,proxy):
-    def new_connection(self,stream ,address,proxy):
-        """
-        Override the maproxy.session.Session.new_connection() function
-        This function is called by the framework (proxyserver) for every new session
-        """
-        # Let's increment the "autonumber" (remember: this is single-threaded, so on lock is required)
-        LoggingSession.running_counter+=1
-        self.connid=LoggingSession.running_counter
-        print("#%-3d: New Connection on %s" % (self.connid,address))
-        super(LoggingSession,self).new_connection(stream,address,proxy)
-        
-    def on_p2s_done_connect(self):
-        """
-        Override the maproxy.session.Session.on_p2s_done_connect() function
-        This function is called by the framework (proxyserver) when the session is 
-        connected to the target-server
-        """
-        print("#%-3d: Server connected" % (self.connid))
-        super(LoggingSession,self).on_p2s_done_connect()
-        
-
-
-    def on_c2p_done_read(self,data):
-        """
-        Override the maproxy.session.Session.on_c2p_done_read(data) function
-        This function is called by the framework (proxyserver) when we get data from the client 
-        (to the target-server)
-        """
-        # First, let's call the parent-class function (on_cp2_done_read),
-        # this will minimize network delay and complete the operation
-        super(LoggingSession,self).on_c2p_done_read(data)
-
-        # Now let simply print the data (print just the printable characters)
-        print("#%-3d:C->S (%d bytes):\n%s" % (self.connid,len(data),filter(lambda x: x in string.printable, data)) )
-        
-
-    def on_p2s_done_read(self,data):
-        """
-        Override the maproxy.session.Session.on_p2s_done_read(data) function
-        This function is called by the framework (proxyserver) when we get data from the server 
-        (to the client)
-        """
-        # First, let's call the parent-class function (on_p2s_done_read),
-        # this will minimize network delay and complete the operation
-        super(LoggingSession,self).on_p2s_done_read(data)
-        
-        # Now let simply print the data (print just the printable characters)
-        print("#%-3d:C<-S (%d bytes):\n%s" % (self.connid,len(data),filter(lambda x: x in string.printable, data)) )
-    
-    def on_c2p_close(self):
-        """
-        Override the maproxy.session.Session.on_c2p_close() function.
-        This function is called by the framework (proxyserver) when the client closes the connection
-        """
-        print("#%-3d: C->S Closed" % (self.connid))
-        super(LoggingSession,self).on_c2p_close()
-
-    def on_p2s_close(self):
-        """
-        Override the maproxy.session.Session.on_p2s_close() function.
-        This function is called by the framework (proxyserver) when the server closes the connection
-        """
-        print("#%-3d: C<-S Closed" % (self.connid))
-        super(LoggingSession,self).on_p2s_close()
-        
-
-
-
-
-class LoggingSessionFactory(maproxy.session.SessionFactory):
-    """
-    This session-factory will be used by the proxy when new sessions
-    need to be generated .
-    We only need a "new" function that will generate a session object
-    that derives from maproxy.session.Session.
-    The session that we create is our lovely LoggingSession that we declared
-    earlier
-    """
-    def __init__(self):
-        super(LoggingSessionFactory,self).__init__()
-    def new(self,*args,**kwargs):
-        return LoggingSession(*args,**kwargs)
-        
-
-
-
-# HTTP->HTTP
-# On your computer, browse to "http://127.0.0.1:81/" and you'll get http://www.google.com
-# The only "special" argument is the "session_factory" that ponits to a new instance of LoggingSessionFactory.
-# By using our special session-factory, the proxy will create the 
-# LoggingSession instances (instead of default Session instances)
-server = maproxy.proxyserver.ProxyServer("http://www.http2demo.io",80,session_factory=LoggingSessionFactory())
-server.listen(8100)
-print("http://127.0.0.1:8100 -> http://localhost")    
-tornado.ioloop.IOLoop.instance().start()
+if __name__ == '__main__':
+    options.parse_command_line()
+    run_proxy(options.port, options.bind)
